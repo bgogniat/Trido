50f2c55069eb31054cae0c3cc147a596ba6137b3
bertrandPiccard <bertrand.gogniat@he-arc.ch>
2021-04-14 11:04:39 +0300

    Test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App started</Text>
+      <Text>App started changed</Text>
       <StatusBar style="auto" />
     </View>
   );
